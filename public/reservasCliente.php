d119c9ee52c5727768afa942851cb2b71f18daa3
unknown <josegoviaramirez@gmail.com>
2025-06-03 18:25:07 +0200

    reservasCliente.php responsive

--- public/reservasCliente.php
+++ public/reservasCliente.php
@@ -33,63 +33,58 @@
 
     $cliente = $crud->obtener("clientes", "where email = \"$_SESSION[cliente]\"")[0];
     echo "<h2 class=\"d-flex justify-content-center py-2\" id=\"bienvenido\">Lista de reservas de $cliente[nombre]</h2>";
+    require_once "../vista/template/navCliente.php";
 ?>
 
-    <!-- Creamos un container en el que estará la barra de navegación y el contenido principal de la página -->
-    <div class="container-fluid">
-        <div class="row">
-            <!-- La barra de navegación será la primera columna -->
-            <?php require_once "../vista/template/navCliente.php"; ?>
-
-            <!-- El contenido principal de la página será la segunda columna -->
-            <div class="col d-flex align-items-center">
-                <?php
-                    $reservas = $crud->listar("*", "reservas", "where cliente = \"$_SESSION[cliente]\"");
-                    if($reservas != null){
-                        ?>
-                        <table class="table table-striped table-hover">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Fecha</th>
-                                    <th scope="col">Hora de inicio</th>
-                                    <th scope="col">Hora de finalización</th>
-                                    <th scope="col">Pista</th>
-                                    <th scope="col">Cancelar reserva</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php
-                                    $horaActual = strtotime("now"); 
-                                    foreach($reservas as $reserva){
-                                        echo "<tr>";
-                                            echo "<td>$reserva[fecha]</td>";
-                                            echo "<td>$reserva[horaInicio]</td>";
-                                            echo "<td>$reserva[horaFin]</td>";
-                                            echo "<td>$reserva[pista]</td>";
-                                            echo "<form method=\"post\" action=\"../servidor/actualizarCalendario.php\">";
-                                            echo "<input name=\"fecha\" type=\"hidden\" value=\"$reserva[fecha]\">";
-                                            echo "<input name=\"horaInicio\" type=\"hidden\" value=\"$reserva[horaInicio]\">";
-                                            echo "<input name=\"pista\" type=\"hidden\" value=\"$reserva[pista]\">";
-                                            $horaReserva = strtotime($reserva['fecha']) + (explode(":", $reserva['horaInicio'])[0] * 60* 60);
-                                            // Si todavía no ha pasado la fecha se permite cancelar la reserva
-                                            if($horaReserva > $horaActual) {
-                                                echo "<td><i class=\"bi bi-x-circle\"></i></td>";
-                                            }
-                                            else {
-                                                echo "<td>Fecha pasada</td>";
-                                            }
-                                            echo "</form>";
-                                        echo "</tr>";
-                                    }
-                                ?>
-                            </tbody>
-                        </table>
-                <?php
-                    }
+    <!-- El contenido principal de la página será la segunda columna -->
+    <div class="col-12 col-sm-6 col-md-7 col-lg-8">
+        <?php
+            $reservas = $crud->listar("*", "reservas", "where cliente = \"$_SESSION[cliente]\"");
+            if($reservas != null){
                 ?>
-            </div>
-        </div>
+                <table class="table table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">Fecha</th>
+                            <th scope="col">Hora de inicio</th>
+                            <th scope="col">Hora de finalización</th>
+                            <th scope="col">Pista</th>
+                            <th scope="col">Cancelar reserva</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                            $horaActual = strtotime("now"); 
+                            foreach($reservas as $reserva){
+                                echo "<tr>";
+                                    echo "<td>$reserva[fecha]</td>";
+                                    echo "<td>$reserva[horaInicio]</td>";
+                                    echo "<td>$reserva[horaFin]</td>";
+                                    echo "<td>$reserva[pista]</td>";
+                                    echo "<form method=\"post\" action=\"../servidor/actualizarCalendario.php\">";
+                                    echo "<input name=\"fecha\" type=\"hidden\" value=\"$reserva[fecha]\">";
+                                    echo "<input name=\"horaInicio\" type=\"hidden\" value=\"$reserva[horaInicio]\">";
+                                    echo "<input name=\"pista\" type=\"hidden\" value=\"$reserva[pista]\">";
+                                    $horaReserva = strtotime($reserva['fecha']) + (explode(":", $reserva['horaInicio'])[0] * 60* 60);
+                                    // Si todavía no ha pasado la fecha se permite cancelar la reserva
+                                    if($horaReserva > $horaActual) {
+                                        echo "<td><i class=\"bi bi-x-circle\"></i></td>";
+                                    }
+                                    else {
+                                        echo "<td>Fecha pasada</td>";
+                                    }
+                                    echo "</form>";
+                                echo "</tr>";
+                            }
+                        ?>
+                    </tbody>
+                </table>
+        <?php
+            }
+        ?>
     </div>
+</div>
+</div>
 
     <?php
         // Cargamos el pie
