c239d9fceda7c56679bef761173e8488b3baa0c3
unknown <josegoviaramirez@gmail.com>
2025-06-12 10:26:36 +0200

    Eliminación de un archivo temporal de configuración y comentarios en verificar.php

--- verificar.php
+++ verificar.php
@@ -5,23 +5,24 @@
     require_once $_SERVER['DOCUMENT_ROOT'] . "/controlador/Crud.php";
     require_once $_SERVER['DOCUMENT_ROOT'] . "/vista/template/header.php";
 
+    // Si hemos recibido el correo y el código del usuario
     if(isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['codigo']) && !empty($_GET['codigo'])){
         $crud = new Crud(new DB("proyecto"));
 	    $devolver = $crud->listar("email, codigo", "clientes", "where email = \"$_GET[email]\" and codigo = \"$_GET[codigo]\" and activo = 0");
-        // Si tenemos una coincidencia
+        // Si tenemos una coincidencia, significa que hay que validar el usuario
         if(count($devolver) > 0){
             $crud->actualizar("clientes", "activo = 1", "where email = \"$_GET[email]\" and codigo = \"$_GET[codigo]\" and activo = 0");
-            echo '<div class="statusmsg">Ha activado el usuario con email ' . $_GET['email'] . '</div>';
+            echo '<div>Ha activado el usuario con email ' . $_GET['email'] . '</div>';
         } 
         // Si no hay una coincidencia
         else {
-            echo '<div class="statusmsg">La URL no es válida o la cuenta ya está activa.</div>';       
+            echo '<div>La URL no es válida o la cuenta ya está activa.</div>';       
         }
     } 
+    // Si no se ha recibido el correo y el código del usuario, es porque no hemos accedido de la manera adecuada a esta página
     else {
-        echo '<div class="statusmsg">Por favor, usa el enlace que se le ha enviado al email.</div>';
+        echo '<div>Por favor, usa el enlace que se le ha enviado al email.</div>';
     }
-
 
     // Cargamos el pie
     require_once $_SERVER['DOCUMENT_ROOT'] . "/vista/template/footer.php";
