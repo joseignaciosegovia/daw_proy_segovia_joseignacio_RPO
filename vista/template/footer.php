67c9c60f2a7f47913c3285ad38d01369f368981a
unknown <josegoviaramirez@gmail.com>
2026-05-20 11:54:36 +0200

    Mejora en el título de la bienvenida

--- vista/template/footer.php
+++ vista/template/footer.php
@@ -1,35 +1,35 @@
-            <footer>
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col footer-col" id="telefonos">
-                            <strong>Teléfonos:</strong>
-                            <ul class="nav flex-column">
-                                <li class="nav-item mb-2">Ciudad deportiva: 656 539 016</li>
-                                <li class="nav-item mb-2">Polideportivo: 926 319 495</li>
-                            </ul>
-                        </div>
-                        <div class="col footer-col" id="correosElectronicos">
-                            <strong>Correo electrónico:</strong>
-                            <ul class="nav flex-column">
-                                <li class="nav-item mb-2">Ciudad deportiva: Ciudaddeportivamoral@gmail.com</li>
-                            </ul>
-                        </div>
-                        <div class="col footer-col" id="paginaWeb">
-                            <strong>Página web:</strong>
-                            <ul class="nav flex-column">
-                                <li><a href="https://www.moraldecalatrava.org/web1/ciudad-deportiva-moral-de-calatrava/" target="_blank">Ciudad Deportiva</a></li>
-                            </ul>
-                        </div>
+        <footer>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col footer-col" id="telefonos">
+                        <strong>Teléfonos:</strong>
+                        <ul class="nav flex-column">
+                            <li class="nav-item mb-2">Ciudad deportiva: 656 539 016</li>
+                            <li class="nav-item mb-2">Polideportivo: 926 319 495</li>
+                        </ul>
+                    </div>
+                    <div class="col footer-col" id="correosElectronicos">
+                        <strong>Correo electrónico:</strong>
+                        <ul class="nav flex-column">
+                            <li class="nav-item mb-2">Ciudad deportiva: Ciudaddeportivamoral@gmail.com</li>
+                        </ul>
+                    </div>
+                    <div class="col footer-col" id="paginaWeb">
+                        <strong>Página web:</strong>
+                        <ul class="nav flex-column">
+                            <li><a href="https://www.moraldecalatrava.org/web1/ciudad-deportiva-moral-de-calatrava/" target="_blank">Ciudad Deportiva</a></li>
+                        </ul>
                     </div>
                 </div>
-            </footer>
-            <!-- JQuery -->
-            <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-            <!-- Bootstrap -->
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
-            <!-- Cada página añadirá los scripts que necesite -->
-            <?php if (function_exists('añadirScriptsPie')){
-                añadirScriptsPie();
-            }?>
-        </body>
+            </div>
+        </footer>
+        <!-- JQuery -->
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+        <!-- Bootstrap -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+        <!-- Cada página añadirá los scripts que necesite -->
+        <?php if (function_exists('añadirScriptsPie')){
+            añadirScriptsPie();
+        }?>
+    </body>
 </html>
