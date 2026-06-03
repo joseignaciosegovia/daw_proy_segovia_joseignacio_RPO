397df8402baf3c2169888bc86a4561777165b84f
unknown <josegoviaramirez@gmail.com>
2026-06-03 12:59:54 +0200

    Problema correo en el servidor5

--- config.php
+++ config.php
@@ -5,30 +5,34 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 $dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__);
-$dotenv->safeLoad(); 
+$dotenv->safeLoad();
 
 function enviarCorreo(string $destinatario, string $asunto, string $cuerpoHtml): bool {
-    $mail = new PHPMailer(true);
-    try {
-        $mail->isSMTP();
-        $mail->Host       = $_ENV['MAILTRAP_HOST'];
-        $mail->SMTPAuth   = true;
-        $mail->Username   = $_ENV['MAILTRAP_USERNAME'];
-        $mail->Password   = $_ENV['MAILTRAP_PASSWORD'];
-        $mail->Port       = $_ENV['MAILTRAP_PORT'];
+    $body = json_encode([
+        'from'    => ['email' => 'mailtrap@example.com', 'name' => 'Reservas Moral de Calatrava'],
+        'to'      => [['email' => $destinatario]],
+        'subject' => $asunto,
+        'html'    => $cuerpoHtml,
+    ]);
 
-        $mail->setFrom('reservas@moraldecalatrava.es', 'Reservas Moral de Calatrava');
-        $mail->addAddress($destinatario);
+    $ch = curl_init('https://sandbox.api.mailtrap.io/api/send/' . $_ENV['MAILTRAP_INBOX_ID']);
+    curl_setopt_array($ch, [
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_POST           => true,
+        CURLOPT_POSTFIELDS     => $body,
+        CURLOPT_HTTPHEADER     => [
+            'Authorization: Bearer ' . $_ENV['MAILTRAP_TOKEN'],
+            'Content-Type: application/json',
+        ],
+    ]);
 
-        $mail->isHTML(true);
-        $mail->CharSet = 'UTF-8';
-        $mail->Subject = $asunto;
-        $mail->Body    = $cuerpoHtml;
+    $response = curl_exec($ch);
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
 
-        $mail->send();
-        return true;
-    } catch (Exception $e) {
-        error_log("Error al enviar correo: {$mail->ErrorInfo}");
+    if ($httpCode !== 200) {
+        error_log("Error al enviar correo: HTTP $httpCode - $response");
         return false;
     }
+    return true;
 }
