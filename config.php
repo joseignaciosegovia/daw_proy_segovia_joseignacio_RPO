8c1f017218d7278dbceb0d6c97c0b5c209aeca5d
unknown <josegoviaramirez@gmail.com>
2026-06-03 13:22:17 +0200

    Problema correo en el servidor11

--- config.php
+++ config.php
@@ -15,24 +15,24 @@ function enviarCorreo(string $destinatario, string $asunto, string $cuerpoHtml):
         'html'    => $cuerpoHtml,
     ]);
 
-    error_log("TOKEN: " . (getenv('MAILTRAP_TOKEN') ?: 'VACIO'));
-error_log("INBOX: " . (getenv('MAILTRAP_INBOX_ID') ?: 'VACIO'));
-
-    $ch = curl_init('https://sandbox.api.mailtrap.io/api/send/inboxes/' . (getenv('MAILTRAP_INBOX_ID') ?: $_ENV['MAILTRAP_INBOX_ID']));
+    $ch = curl_init('https://sandbox.api.mailtrap.io/api/send/' . (getenv('MAILTRAP_INBOX_ID') ?: $_ENV['MAILTRAP_INBOX_ID']));
     curl_setopt_array($ch, [
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_POST           => true,
         CURLOPT_POSTFIELDS     => $body,
         CURLOPT_FOLLOWLOCATION => true,
         CURLOPT_HTTPHEADER     => [
-            'Authorization: Bearer ' . (getenv('MAILTRAP_TOKEN') ?: $_ENV['MAILTRAP_TOKEN']),
+            'Api-Token: ' . (getenv('MAILTRAP_TOKEN') ?: $_ENV['MAILTRAP_TOKEN']),
             'Content-Type: application/json',
         ],
     ]);
 
+    error_log("URL: " . curl_getinfo($ch, CURLINFO_EFFECTIVE_URL));
+
     $response = curl_exec($ch);
     $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    curl_close($ch);
+    error_log("HTTP: " . curl_getinfo($ch, CURLINFO_HTTP_CODE));
+error_log("RESPUESTA: " . $response);
 
     if ($httpCode !== 200) {
         error_log("Error al enviar correo: HTTP $httpCode - $response");
